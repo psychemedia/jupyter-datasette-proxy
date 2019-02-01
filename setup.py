42f032591cce789762b69c2bacfc0636ec15fdae
Tony Hirst <tony.hirst@gmail.com>
2019-02-01 21:58:57 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+  name="jupyter-datsette-server",
+  # py_modules rather than packages, since we only have 1 file
+  py_modules=['nbdatasette'],
+  entry_points={
+      'jupyter_serverproxy_servers': [
+          'Datasette = nbdatasette:setup_datasette',
+      ]
+  },
+)
