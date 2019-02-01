d917603f91b60a644cf4e08f6116cd9e91cec369
Tony Hirst <tony.hirst@gmail.com>
2019-02-01 22:09:52 +0000

    Create nbdatasette.py

---
+++ nbdatasette.py
@@ -0,0 +1,14 @@
+import os
+
+def setup_datasette():
+  dbpath = os.path.join(os.environ['HOME'], 'dakar_sql.sqlite')
+  return {
+    'command': ['datasette', 'serve', '-p', '{port}', dbpath],
+    #The following needs a the labextension installing.
+    #eg in postBuild: jupyter labextension install jupyterlab-server-proxy
+    'launcher_entry': {
+        'enabled': True,
+        #'icon_path': '/home/jovyan/.jupyter/datasette.svg',
+        'title': 'datasette',
+    },
+  }
