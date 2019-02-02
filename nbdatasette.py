1d3fa09bda964adce104b88b1f4f44af448f8ad7
Tony Hirst <tony.hirst@gmail.com>
2019-02-02 00:56:14 +0000

    Update nbdatasette.py

--- nbdatasette.py
+++ nbdatasette.py
@@ -6,7 +6,9 @@ def setup_datasette():
     #datasette does look like it supports static references, but I'm missing something somewhere?
     # https://github.com/simonw/datasette/issues/160
     #If we set static refererncing, do we also need ;absolute_url': False ?
-    #I may (also) be misunderstanding {base_url}
+    #I may (also) be misunderstanding {base_url}. Is that the domain, the domain+path, or the path?
+    #If it's the path, then can it be used to set the static element (once I figure out what args --static takes)
+    #I am so in the land of so-tired I have no idea what anything does.
     #'command': ['datasette', 'serve', '-p', '{port}', '--static', 'static:{base_url}', dbpath],
     'command': ['datasette', 'serve', '-p', '{port}', dbpath],
     'absolute_url': True,
