5248c189cc35a00ae7ad52e91eaf2cf355eb623f
YuviPanda <yuvipanda@gmail.com>
2022-06-22 16:27:44 -0700

    Rename to jupyter_datasette_proxy
    - Naming convention fits better with other helper packages
      like this
    - Depend on jupyter-server-proxy and datasette
    - Use --setting instead of --config, as --config is deprecated
    - Don't pass -d, instead it'll just start on whatever the cwd of
      the notebook process is. This matches other proxy helpers
    - Add to README
    - Set version to 1.0
    - Populate long_description in setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,24 @@
 import setuptools
 
+with open("README.md", encoding="utf8") as f:
+    readme = f.read()
+
 setuptools.setup(
-  name="jupyter-datsette-server",
+  name="jupyter-datasette-proxy",
+  version="1.0",
   # py_modules rather than packages, since we only have 1 file
   py_modules=['nbdatasette'],
+  install_requires=[
+    'datasette',
+    'jupyter-server-proxy'
+  ],
+  description="Run datasette on JupyterHub with jupyter-server-proxy",
+  long_description=readme,
+  long_description_content_type="text/markdown",
   entry_points={
       'jupyter_serverproxy_servers': [
           #path = module:entry_function
-          'datasette = nbdatasette:setup_datasette',
+          'datasette = jupyter_datasette_proxy:setup_datasette',
       ]
   },
 )
